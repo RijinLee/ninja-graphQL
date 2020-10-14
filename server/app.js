a871fd4b81869df88a89f8ef9ed55e07a72f1a82
Rijin Lee <rijinlee@gmail.com>
2020-10-13 23:07:13 -0400

    added MongoDB/Mongoose ORM schema

--- server/app.js
+++ server/app.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect(
+  "mongodb+srv://rijin:test123@cluster0.a4wj2.mongodb.net/graphql-ninja?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB Atlas");
+});
 
 // telling express to use this schema to map out the graph
 app.use(
@@ -14,5 +23,5 @@ app.use(
 );
 
 app.listen(4000, () => {
-  console.log(`listening for requests on port 4000`);
+  console.log(`Listening for requests on port 4000`);
 });
