aace26f7161a5283b16b6070051200a28a50d4d5
Rijin Lee <rijinlee@gmail.com>
2020-10-15 10:29:35 -0400

    create-react-app

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// components
+import BookList from "./components/BookList";
+
+function App() {
+  return (
+    <div className="main">
+      <h1>Ninja's Reading List</h1>
+      <BookList />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/BookList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class BookList extends React.Component {
+  render() {
+    return (
+      <div>
+        <ul id="book-list">
+          <li>Book name</li>
+        </ul>
+      </div>
+    );
+  }
+}
